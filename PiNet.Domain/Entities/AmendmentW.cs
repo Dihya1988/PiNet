e5ea4573dc25c56f9130296d0a9c1e2b53fbc815
Nouha <Dihya1988@users.noreply.github.com>
2018-05-23 15:57:07 +0100

    Initial commit

---
+++ PiNet.Domain/Entities/AmendmentW.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PIdotnet.Domain.Entities
+{
+    class AmendmentW
+    {
+    }
+}
---
+++ PiNet.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PiNet.Web.Startup))]
+namespace PiNet.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
